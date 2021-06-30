707cb0675b313a00ae749e8421e4e41c6b5000da
mrallentam <47073620+mrallentam@users.noreply.github.com>
2021-06-30 08:51:13 -0700

    Delete Scripts directory

--- Scripts/ADFS/ADFSRestoreClaims.ps1
+++
@@ -1,8 +0,0 @@
-$Relay = 'relaying party trust name'
-$backupfile = 'c:\path\To\backup.txt.txt'
- 
-#Read the claim backup
-[string]$Backup = Get-Content $backupfile
- 
-#overwrite with backup
-Set-AdfsRelyingPartyTrust -TargetName $relay -IssuanceTransformRules $Backup
