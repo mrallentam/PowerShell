240db7123b3c16359d284347bfa6c924eb8a2ca4
mrallentam <47073620+mrallentam@users.noreply.github.com>
2021-07-13 12:50:28 -0700

    Add files via upload

---
+++ Scripts/WSUS/INVOKE-WURunAutoApproval.ps1
@@ -0,0 +1,10 @@
+﻿##Run Auto Approval rules
+
+$wsus=[Microsoft.UpdateServices.Administration.AdminProxy]::GetUpdateServer()
+
+#Get and run auto approve rules
+$rules = $wsus.GetInstallApprovalRules()
+foreach ($rule in $rules){
+$rule.Enabled = $true
+$rule.ApplyRule()
+}
---
+++ Scripts/WSUS/INVOKE-WUSynchronize.ps1
@@ -0,0 +1,19 @@
+﻿
+#Get Update Server
+$wsus=[Microsoft.UpdateServices.Administration.AdminProxy]::GetUpdateServer()
+
+#Get Configuration
+$wsus.GetConfiguration()
+
+#Get Subscription
+$wsus.GetSubscription()
+
+#Synchronize!
+$wsus.GetSubscription().StartSynchronization()
+
+#Check progress
+#$wsus.GetSubscription().GetSynchronizationProgress()
+
+#Get Last Sync
+#$wsus.GetSubscription().GetLastSynchronizationInfo()
+
