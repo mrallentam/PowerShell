8c490ef9840240522548824e1127c9d62087a3c1
mrallentam <47073620+mrallentam@users.noreply.github.com>
2021-07-28 03:37:58 -0700

    Add files via upload

---
+++ Scripts/Red Forest Infrastructure Build/ElevationScripts/elevate-shadowprincipal.ps1
@@ -0,0 +1,27 @@
+﻿#Simple Elevation script with input box and logging
+#Written by Allen Tam
+
+#Inputbox
+function Read-InputBox([string]$Message, [string]$WindowTitle)
+{
+    Add-Type -AssemblyName Microsoft.VisualBasic
+    return [Microsoft.VisualBasic.Interaction]::InputBox($Message, $WindowTitle)
+}
+
+#Script
+#get loggedin username
+$user = $env:USERNAME
+$path = path_to_log
+
+#static shadow principal per domain
+$blueforestshadowprincipal = "CN=BLUE-Domain Admins,CN=Shadow Principal Configuration,CN=Services,CN=Configuration,DC=DOMAIN,DC=REDFOREST,DC=com"
+
+[int]$HOURS = Read-InputBox -Message "Please enter up to 2-8 hours of Domain Admin Elevation" -WindowTitle "Please Enter Number of Elevation Hours" 
+
+#Set Shadow principal member duration and write log
+$duration= $hours * 60
+
+Set-ADObject -Identity $blueforestshadowprincipal -Add @{'member'="<TTL=$duration,CN= $username,OU=PROD-Shadow,DC=DOMAIN,DC=REDFOREST,DC=com>"}
+$datetime=get-date
+add-content -path $path -value "$user elevated at $datetime for $duration hours"
+
---
+++ Scripts/Red Forest Infrastructure Build/ElevationScripts/elevate-shortlivedgroupmembership.ps1
@@ -0,0 +1,22 @@
+﻿#elevate to nested group with inputbox and logging
+#written by Allen Tam
+#Inputbox
+function Read-InputBox([string]$Message, [string]$WindowTitle)
+{
+    Add-Type -AssemblyName Microsoft.VisualBasic
+    return [Microsoft.VisualBasic.Interaction]::InputBox($Message, $WindowTitle)
+}
+
+#elevation duration
+[int]$hours = Read-InputBox -Message "Please enter up to 2-8 hours of Domain Admin Elevation" -WindowTitle "Please Enter Number of Elevation Hours" 
+$duration= $hours * 60
+
+#get username
+$user = $env:USERNAME
+$path = "path to log"
+
+
+$Time = New-TimeSpan -Minutes $duration 
+Add-ADGroupMember -Identity ‘$name of nestedgroup’ -Members $user -MemberTimeToLive $Time
+$date = get-date
+add-content -path $path -value "$user elevated for $hours hours at $date"
