bdf9a9e5123cbebe42e27f6c226d5ab113a57e23
mrallentam <47073620+mrallentam@users.noreply.github.com>
2021-09-07 15:16:04 -0700

    Add files via upload

---
+++ Scripts/ActiveDirectory/get-emptygpos.ps1
@@ -0,0 +1,17 @@
+﻿#Get Empty GPOs
+mkdir c:\temp\emptygpos
+$emptygpocsv="c:\temp\emptygpos\emptygpos.csv"
+$domain=read-host -prompt "Enter FQDN of Domain"
+$EmptyGPO = New-Object System.Collections.ArrayList
+$GPOs = Get-GPO -domain $domain -All  
+Write-Verbose -Message "Found '$($GPOs.Count)'"
+ForEach ($gpo  in $GPOs) { 
+        [xml]$GPOXMLReport = $gpo | Get-GPOReport -ReportType xml
+        if ($null -eq $GPOXMLReport.gpo.User.ExtensionData -and $null -eq $GPOXMLReport.gpo.Computer.ExtensionData) {
+            $EmptyGPO += $gpo
+        }
+    }
+    if (($EmptyGPO).Count -ne 0) {
+
+    $EmptyGPO.DisplayName| export-csv -path $emptygpocsv -append -NoTypeInformation
+    }
---
+++ Scripts/ActiveDirectory/get-unlinkedgpos.ps1
@@ -0,0 +1,16 @@
+﻿#Get unlinked GPOs
+mkdir c:\temp\unlinkedgpos
+$csvfilepath="c:\temp\unlinkedgpos\unlinkedgpos.csv"
+$domain=read-host -prompt "Enter FQDN of Domain"
+$UnlinkedGPO = New-Object System.Collections.ArrayList
+ $AllGPOs = Get-GPO -domain $domain -All  
+    ForEach ($1gpo  in $AllGPOs) { 
+        Write-Verbose -Message "Checking '$($1gpo.DisplayName)' link"
+        [xml]$GPOXMLReport = $1gpo | Get-GPOReport -ReportType xml
+        if ($null -eq $GPOXMLReport.GPO.LinksTo) { 
+            $UnlinkedGPO += $1gpo
+        }
+    }
+    if (($UnlinkedGPO).Count -ne 0) {
+    $UnlinkedGPO.DisplayName| export-csv -path $csvfilepath -append -NoTypeInformation
+    }
