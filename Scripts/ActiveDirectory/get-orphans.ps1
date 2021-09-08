d255962812c4c520143e7af63fefd035e1c8f0ab
mrallentam <47073620+mrallentam@users.noreply.github.com>
2021-09-08 08:32:46 -0700

    Add files via upload

---
+++ Scripts/ActiveDirectory/get-orphans.ps1
@@ -0,0 +1,12 @@
+﻿
+$domain="domain"
+$gpos = Get-GPO -All -Domain $domain|Select @{ n='GUID'; e = {'{' + $_.Id.ToString().ToUpper() + '}'}}|Select -ExpandProperty GUID
+$polPath = "\\$domain\SYSVOL\$domain\Policies\"
+$folders = Get-ChildItem $polPath -Exclude 'PolicyDefinitions'
+$gpoArray = $gpos.GUID
+       ForEach ($folder in $folders) {
+            if (-not $gpos.contains($folder.Name)) {
+                $orphaned += $folder|out-file c:\temp\orphans.txt -append
+            }
+        }
+ 
